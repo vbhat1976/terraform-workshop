7d7518adf13b98d2e3442d1ade8f473d3d478b10
Patrick Force <patrickforce@gmail.com>
2019-06-18 21:20:49 -0600

    update student bucket name

--- student-environments/main.tf
+++ student-environments/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "student_buckets" {
   count  = "${length(var.student_aliases)}"
-  bucket = "rockholla-di-${element(var.student_aliases, count.index)}-store"
+  bucket = "rockholla-di-${element(var.student_aliases, count.index)}"
   acl    = "private"
   region = "${var.aws_region}"
 }
