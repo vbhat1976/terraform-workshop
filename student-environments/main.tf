fcbeb23aa8170caba5edaffbf857d03d7be08fce
Patrick Force <patrickforce@gmail.com>
2019-06-18 22:42:25 -0600

    add back force_destroy to bucket resource

--- student-environments/main.tf
+++ student-environments/main.tf
@@ -6,10 +6,11 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "student_buckets" {
-  count  = "${length(var.student_aliases)}"
-  bucket = "rockholla-di-${element(var.student_aliases, count.index)}"
-  acl    = "private"
-  region = "${var.aws_region}"
+  count         = "${length(var.student_aliases)}"
+  bucket        = "rockholla-di-${element(var.student_aliases, count.index)}"
+  acl           = "private"
+  region        = "${var.aws_region}"
+  force_destroy = true
 }
 
 resource "aws_iam_user" "students" {
