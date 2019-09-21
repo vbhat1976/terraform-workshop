d81fa43ad5df9bc0759c339a1518f3211d550cae
rockholla <patrickforce@gmail.com>
2019-09-21 15:41:16 -0700

    update print users output format

--- student-environments/print-users.sh
+++ student-environments/print-users.sh
@@ -10,8 +10,8 @@ for username in $(echo $values | jq -r '.students.value[].name'); do
   echo "AWS Console Password:  $password"
   region=$(echo $values | jq -r '.students.value['"$i"'].region')
   echo "Exercise 11 Region:    $region"
+  echo "Link to the slides:    https://bit.ly/2KExiVp"
+  echo "Instructor email:      patrick+di@rockholla.org"
   echo ""
-  echo "Link to the slides: https://bit.ly/2KExiVp"
-  echo "Instructor email: patrick+di@rockholla.org"
   let i=i+1
 done
