d3e373da2d362aae55f78b09218705fd9bcf83d9
Dave Wade-Stein <dws@developintelligence.com>
2020-03-17 13:59:39 -0600

    restructure alternate experiment

--- solution-examples/experiment-01-alt/main.tf
+++ solution-examples/experiment-01-alt/main.tf
@@ -1,78 +1,15 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = var.region
+module "server" {
+  source = "./modules/server"
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_security_group" "nginx" {
-  name        = "nginx_firewall"
-  description = "Firewall for the nginx-server"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "tls_private_key" "nginx_server" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "aws_key_pair" "nginx_server" {
-  key_name   = "nginx_server"
-  public_key = tls_private_key.nginx_server.public_key_openssh
-}
-
-resource "aws_instance" "nginx_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-
-  key_name = aws_key_pair.nginx_server.key_name
-
-  security_groups = [aws_security_group.nginx.name]
-
+resource "null_resource" "always-run-provider-script" {
   provisioner "remote-exec" {
     connection {
       type        = "ssh"
-      host        = self.public_ip
+      host        = module.server.instance_public_ip
       user        = "ubuntu"
-      private_key = tls_private_key.nginx_server.private_key_pem
+      private_key = module.server.private_key
     }
     scripts = ["./provisioner.sh"]
   }
-
-  tags = {
-    Name = "nginx-server"
-  }
 }
-
