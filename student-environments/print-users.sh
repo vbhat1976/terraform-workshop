85f2e7e5cf7e84a37d33c9d9f64a8ab7cabe5860
Dave Wade-Stein <dws@developintelligence.com>
2020-03-19 20:10:32 -0600

    some script mods for VILT

--- student-environments/print-users.sh
+++ student-environments/print-users.sh
@@ -4,18 +4,20 @@ values=$(terraform output -json)
 
 let i=0
 for username in $(echo $values | jq -r '.students.value[].name'); do
-  echo "Instructions repo:     https://github.com/davewadestein/terraform-workshop"
-  echo "Console URL:           https://introterraform.signin.aws.amazon.com/console"
-  echo "Username/Alias:        $username"
-  password=$(echo $values | jq -r '.passwords.value[]['"$i"']' | base64 --decode | gpg -dq)
-  echo "AWS Console Password:  $password"
-  region=$(echo $values | jq -r '.students.value['"$i"'].region')
-  echo "Exercise 11 Region:    $region"
-  echo "Link to the slides:    ..."
-  echo "Instructor email:      dave@developintelligence.com"
-  #echo "Course Evaluation:     $(cat survey-link)"
-  echo ""
-  echo ""
-  echo ""
-  let i=i+1
+  for loop in 1; do
+	echo "Instructions repo:     https://github.com/davewadestein/terraform-workshop"
+	echo "Console URL:           https://introterraform.signin.aws.amazon.com/console"
+	echo "Username/Alias:        $username"
+	password=$(echo $values | jq -r '.passwords.value[]['"$i"']' | base64 --decode | gpg -dq)
+	echo "AWS Console Password:  $password"
+	region=$(echo $values | jq -r '.students.value['"$i"'].region')
+	echo "Exercise 11 Region:    $region"
+	echo "Link to the slides:    http://bit.ly/terraform-day-1"
+	echo "Instructor email:      dave@developintelligence.com"
+	#echo "Course Evaluation:     $(cat survey-link)"
+	echo ""
+	echo ""
+	echo ""
+	let i=i+1
+  done > tf-user$i
 done
